4623be204933ea3df001c27d05e59c02e1893c29
Tanay Garg <tanay@cloudflare.com>
2025-07-10 18:23:25 -0500

    added support for websockets

--- src/index.ts
+++ src/index.ts
@@ -11,8 +11,32 @@
  * Learn more at https://developers.cloudflare.com/workers/
  */
 
+
+// ! all this is untested code
+async function handleRequest(request: any) {
+	const upgradeHeader = request.headers.get("Upgrade");	
+	if (!upgradeHeader || upgradeHeader.toLowerCase() !== "websocket") {
+		return new Response("Expected websocket", { status: 426 });
+	}
+
+	const webSocketPair = new WebSocketPair();
+	const [client, server] = Object.values(webSocketPair);
+
+	server.accept(); // Accept the server WebSocket and Keep connection alive
+	server.addEventListener('message', (event) => {
+		console.log("Message from client:", event.data);
+		server.send(`Hello from server! You said: ${event.data}`);
+	});
+
+	return new Response(null, {
+		status: 101,
+		webSocket: client,
+	});
+
+}
+
 export default {
 	async fetch(request, env, ctx): Promise<Response> {
-		return new Response('Hello Worker!');
+		return handleRequest(request);
 	},
 } satisfies ExportedHandler<Env>;
