f3ce9c890f60b7d6acef485db5b0bdb79b15e9a7
Robert Lockay <robert.lockay@gmail.com>
2025-09-11 19:14:17 -0500

    added new api route and used route to display basic data from db on homepage

--- worker/src/types.ts
+++ worker/src/types.ts
@@ -6,4 +6,22 @@ declare global {
   }
 }
 
+export type ApiResponse<T> = {
+  success: boolean;
+  message: String;
+  timestamp: String;
+  body?: T;
+}
+
+export type SensorData = {
+  id: number;
+  water_height: number;
+  latitude: number;
+  longitude: number;
+  sensor_altitude: number;
+  timestamp: String;
+  comment: String;
+  sensor_id: number;
+}
+
 export {};
